eb1b7b54f35d0e142014038abd117689c7c518a3
danny-fromhell <daniel19lopez54@gmail.com>
2026-04-04 19:09:34 -0600

    pantallas de: login, registro usuario, recuperar contraseña, registro info personal

---
+++ app/src/main/java/com/example/deckvault/ForgotPasswordFragment.kt
@@ -0,0 +1,29 @@
+package com.example.deckvault
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class ForgotPasswordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnVolverLogin = view.findViewById<Button>(R.id.btnVolverLogin)
+
+        btnVolverLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_forgotPasswordFragment_to_loginFragment)
+        }
+    }
+}
