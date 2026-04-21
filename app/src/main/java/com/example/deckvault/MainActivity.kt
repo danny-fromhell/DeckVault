3796f39d93fce8a654caa0072a0ae6c8a71d83b5
danny-fromhell <daniel19lopez54@gmail.com>
2026-04-21 09:38:01 -0600

    creacion de FragmentCommunicator y animacion de carga

--- app/src/main/java/com/example/deckvault/MainActivity.kt
+++ app/src/main/java/com/example/deckvault/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.deckvault
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+import com.example.deckvault.core.FragmentCommunicator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentCommunicator {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun manageLoader(isVisible: Boolean) {
+        val loadingScreen = findViewById<View>(R.id.loadingScreen)
+        val lottieLoading = findViewById<LottieAnimationView>(R.id.lottieLoading)
+
+        loadingScreen.visibility = if (isVisible) View.VISIBLE else View.GONE
+
+        if (isVisible) {
+            lottieLoading.playAnimation()
+        } else {
+            lottieLoading.cancelAnimation()
+        }
     }
 }
